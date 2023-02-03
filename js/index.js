25f42adf59ce70118f898b4641cdeb8a6cfe1c4d
Casey Walker <casey@moveparallel.com>
2023-02-03 15:29:07 -0800

    log metadata

--- js/index.js
+++ js/index.js
@@ -24,11 +24,24 @@ const startVideo = (stream) => {
 };
 
 /**
+ * Log video frame metadata.
+ */
+const logMetadata = (now, metadata) => {
+  // Do something with the frame.
+  console.log(now, metadata);
+  // Re-register the callback to be notified about the next frame.
+  video.requestVideoFrameCallback(logMetadata);
+};
+
+/**
  * Main script
  */
 const main = async () => {
   const stream = await initStream();
   startVideo(stream);
+
+  // Initially register the callback to be notified about the first frame.
+  video.requestVideoFrameCallback(logMetadata);
 };
 
 main();
