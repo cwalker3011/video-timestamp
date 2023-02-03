d42af288a77a7a0cd9b17e9c02b24131017d5c00
Casey Walker <casey@moveparallel.com>
2023-02-03 15:23:26 -0800

    video streaming

--- js/index.js
+++ js/index.js
@@ -1,1 +1,34 @@
-console.log('hello world');
+/**
+ * HTML video element
+ */
+const video = document.getElementById('video-display');
+
+/**
+ * Initialize webcam stream
+ */
+const initStream = async () => {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({audio: false, video: true});
+    return stream;
+  } catch (e) {
+    console.log(e);
+  };
+};
+
+/**
+ * Stream to video component
+ */
+const startVideo = (stream) => {
+  stream.getVideoTracks();
+  video.srcObject = stream;
+};
+
+/**
+ * Main script
+ */
+const main = async () => {
+  const stream = await initStream();
+  startVideo(stream);
+};
+
+main();
