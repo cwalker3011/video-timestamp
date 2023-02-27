98bef22cb70af4d03a711fe852cee230f3010f84
Casey Walker <casey@moveparallel.com>
2023-02-27 14:15:33 -0800

    set up sender and receiver

--- js/index.js
+++ js/index.js
@@ -1,47 +1,16 @@
 /**
  * HTML video element
  */
-const video = document.getElementById('video-display');
-
-/**
- * Initialize webcam stream
- */
-const initStream = async () => {
-  try {
-    const stream = await navigator.mediaDevices.getUserMedia({audio: false, video: true});
-    return stream;
-  } catch (e) {
-    console.log(e);
-  };
-};
-
-/**
- * Stream to video component
- */
-const startVideo = (stream) => {
-  stream.getVideoTracks();
-  video.srcObject = stream;
-};
-
-/**
- * Log video frame metadata.
- */
-const logMetadata = (now, metadata) => {
-  // Do something with the frame.
-  console.log(now, metadata);
-  // Re-register the callback to be notified about the next frame.
-  video.requestVideoFrameCallback(logMetadata);
-};
+const senderVideo = document.getElementById('video-sender');
+const receiverVideo = document.getElementById('video-receiver');
+const startButton = document.getElementById('start-button');
+const callButton = document.getElementById('call-button');
 
 /**
  * Main script
  */
 const main = async () => {
-  const stream = await initStream();
-  startVideo(stream);
-
-  // Initially register the callback to be notified about the first frame.
-  video.requestVideoFrameCallback(logMetadata);
+  console.log('hello world');
 };
 
 main();
