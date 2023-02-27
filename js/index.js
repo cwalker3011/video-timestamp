918e999b2cf97874839588bc2518dbd777632aef
Casey Walker <casey@moveparallel.com>
2023-02-27 14:29:48 -0800

    init local stream

--- js/index.js
+++ js/index.js
@@ -6,11 +6,25 @@ const receiverVideo = document.getElementById('video-receiver');
 const startButton = document.getElementById('start-button');
 const callButton = document.getElementById('call-button');
 
-/**
- * Main script
- */
-const main = async () => {
-  console.log('hello world');
-};
+let senderStream;
+let pc1;
+let pc2;
 
-main();
+/**
+ * Start call.
+ */
+const start = async () => {
+  console.log('Requesting local stream');
+  startButton.disabled = true;
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({video: true});
+    console.log('Received local stream');
+    senderVideo.srcObject = stream;
+    localStream = stream;
+    callButton.disabled = false;
+  } catch (e) {
+    alert(`getUserMedia() error: ${e}`);
+  }
+}
+
+startButton.addEventListener('click', start);
